a7529c112962fa8623efa8d4136f9a3a1bbba5cc
t.adamski <t.adamski@cogision.com>
2013-11-29 09:33:09 +0100

    WIP: remove function instead of auto remove on hide

--- src/modal/example/js/app.js
+++ src/modal/example/js/app.js
@@ -4,12 +4,15 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['ui.semantic.modal'])
         .controller('sampleCtrl', ['$scope', '$modal', function($scope, $modal) {
+                var modal;
                 $scope.modalOpen = function() {
-                    $modal({template: '<h1>Hello {{world}}</h1>', controller: 'modalController', attrs: {'class': 'ui basic modal'}})
-                            .modal('setting', 'onHide', function() {
-                                console.log("hide param");
-                            })
-                            .modal('show');
+                    if (angular.isDefined(modal)) {
+                        modal.remove();
+                    }
+                    modal = $modal({template: '<h1>Hello {{world}}</h1>', controller: 'modalController', attrs: {'class': 'ui basic modal'}});
+                    modal.modal('setting', 'onHide', function() {
+                        console.log("hide param");
+                    }).modal('show');
                 };
             }])
         .controller('modalController', ['$scope', '$timeout', function($scope, $timeout) {
--- src/modal/modal.js
+++ src/modal/modal.js
@@ -2,59 +2,50 @@
 (function() {
 
 // Declare app level module which depends on filters, and services
-angular.module('ui.semantic.modal', [])
-        .provider('$modal', function() {
-            this.$get = ['$controller', '$compile', '$rootScope', '$log', '$q', '$timeout', function($controller, $compile, $rootScope, $log, $q, $timeout) {
-                    return function(options) {
-                        var modalScope = (options.scope || $rootScope.$new());
-                        var templateScope;
-                        if (options.controller) {
-                            templateScope = modalScope.$new();
-                            $controller(options.controller, {$scope: templateScope})
-                        } else {
-                            templateScope = modalScope;
-                        }
-
-                        var templateDefer = $q.defer();
-                        if (options.template) {
-                            templateDefer.resolve(options.template);
-                        } else if (options.templateUrl) {
-                            $timeout(function() {
-                                templateDefer.resolve('<h1>some template from url, world: {{world}}</h1>')
-                            }, 1000);
-                        }
-
-                        var modalComponent = $('<div class="ui modal"></div>');
-                        templateDefer.promise.then(function(template) {
-                            modalComponent.append($compile(template)(templateScope));
-                        });
-                        if (options.attrs) {
-                            for (var key in options.attrs) {
-                                var value = options.attrs[key];
-                                modalComponent.attr(key, value);
+    angular.module('ui.semantic.modal', [])
+            .provider('$modal', function() {
+                this.$get = ['$controller', '$compile', '$rootScope', '$log', '$q', '$timeout', function($controller, $compile, $rootScope, $log, $q, $timeout) {
+                        return function(options) {
+                            var modalScope = (options.scope || $rootScope.$new());
+                            var templateScope;
+                            if (options.controller) {
+                                templateScope = modalScope.$new();
+                                $controller(options.controller, {$scope: templateScope});
+                            } else {
+                                templateScope = modalScope;
                             }
 
-                        }
-                        var onHideWrap = function(callback) {
-                            return function() {
-                                if (callback) {
-                                    callback.apply(this, arguments);
+                            var templateDefer = $q.defer();
+                            if (options.template) {
+                                templateDefer.resolve(options.template);
+                            } else if (options.templateUrl) {
+                                $timeout(function() {
+                                    templateDefer.resolve('<h1>some template from url, world: {{world}}</h1>')
+                                }, 1000);
+                            }
+
+                            var modalComponent = $('<div class="ui modal"></div>');
+                            if (options.attrs) {
+                                for (var key in options.attrs) {
+                                    var value = options.attrs[key];
+                                    modalComponent.attr(key, value);
                                 }
-                                modalScope.$destroy();
-                                //how to wait till the end of animation??
-                                modalComponent.remove();
+                            }
+                            modalComponent = $compile(modalComponent)(modalScope);
+
+                            templateDefer.promise.then(function(template) {
+                                modalComponent.append($compile(template)(templateScope));
+                            });
+                            return {
+                                modal: function() {
+                                    return modalComponent.modal.apply(modalComponent, arguments);
+                                },
+                                remove: function() {
+                                    modalComponent.remove();
+                                    modalScope.$destroy();
+                                }
                             };
                         };
-                        modalComponent.modal('setting', 'onHide', onHideWrap())
-                        return {
-                            modal: function(argument1, argument2, argument3) {
-                                if (argument1 === 'setting' && argument2 === 'onHide' && argument3) {
-                                    argument3 = onHideWrap(argument3);
-                                }
-                                return modalComponent.modal(argument1, argument2, argument3);
-                            }
-                        };
-                    };
-                }];
-        });
+                    }];
+            });
 })();
