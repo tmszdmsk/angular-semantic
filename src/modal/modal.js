5652196440561ce92b21a1b2a9715894fcb2d80a
Tomasz Adamski <tomasz.adamski@gmail.com>
2014-02-18 15:43:09 +0100

    Update modal.js
    cleaning, removing not needed stuff

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -2,50 +2,24 @@
 (function() {
 
 // Declare app level module which depends on filters, and services
-    angular.module('ui.semantic.modal', [])
-            .provider('$modal', function() {
-                this.$get = ['$controller', '$compile', '$rootScope', '$log', '$q', '$timeout', function($controller, $compile, $rootScope, $log, $q, $timeout) {
-                        return function(options) {
-                            var modalScope = (options.scope || $rootScope.$new());
-                            var templateScope;
-                            if (options.controller) {
-                                templateScope = modalScope.$new();
-                                $controller(options.controller, {$scope: templateScope});
-                            } else {
-                                templateScope = modalScope;
-                            }
-
-                            var templateDefer = $q.defer();
-                            if (options.template) {
-                                templateDefer.resolve(options.template);
-                            } else if (options.templateUrl) {
-                                $timeout(function() {
-                                    templateDefer.resolve('<h1>some template from url, world: {{world}}</h1>')
-                                }, 1000);
-                            }
-
-                            var modalComponent = $('<div class="ui modal"></div>');
-                            if (options.attrs) {
-                                for (var key in options.attrs) {
-                                    var value = options.attrs[key];
-                                    modalComponent.attr(key, value);
-                                }
-                            }
-                            modalComponent = $compile(modalComponent)(modalScope);
-
-                            templateDefer.promise.then(function(template) {
-                                modalComponent.append($compile(template)(templateScope));
-                            });
-                            return {
-                                modal: function() {
-                                    return modalComponent.modal.apply(modalComponent, arguments);
-                                },
-                                remove: function() {
-                                    modalComponent.remove();
-                                    modalScope.$destroy();
-                                }
-                            };
-                        };
-                    }];
-            });
+   angular.module('ui.semantic.modal', [])
+           .service('$modal', ['$controller', '$compile', '$rootScope', function($controller, $compile, $rootScope) {
+                   return function(options) {
+                       var templateScope = (options.scope || $rootScope.$new());
+                       if (options.controller) {
+                           $controller(options.controller, {$scope: templateScope});
+                       }
+                       var modalComponent = $compile(options.template)(templateScope);
+                       return {
+                           modal: function() {
+                               return modalComponent.modal.apply(modalComponent, arguments);
+                           },
+                           remove: function() {
+                               modalComponent.remove();
+                               templateScope.$destroy();
+                           }
+                       };
+                   }
+                   ;
+               }]);
 })();
